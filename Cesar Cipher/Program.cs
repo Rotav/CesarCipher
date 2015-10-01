a81b797ff33ac042fd9035d440bf9d58f9f8b388
Rotav <10harrob@gmail.com>
2015-10-01 16:02:39 +0100

    inital code with bugs

---
+++ Cesar Cipher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            int numPlaces;
+            Console.WriteLine("Please enter the message that you want to encode using cesar cipher.");
+            string message = Console.ReadLine();
+            Console.WriteLine("Please enter the number of places you want each letter to be moved by.");
+            Int32.TryParse(Console.ReadLine(), out numPlaces);
+
+            char[] messageArray = message.ToCharArray();
+            char[] newAlphabet = new char[26];
+            for (int i = 0; i < 26; i++)
+            {
+                newAlphabet[i] = alphabet[i + numPlaces];
+                if (newAlphabet[i] > 26)
+                {
+                    newAlphabet[i] = newAlphabet[i-26];
+                }
+            }
+            Console.WriteLine("Your encoded message is: " + newAlphabet);
+        }
+    }
+}
